eead626dd812fb59c0148daddd53e2d0b3a57c62
RiverNety <89873015+RiverNety@users.noreply.github.com>
2025-11-22 16:41:37 -0300

    Mudança que o J disse para fazer

--- CNNC.py
+++ CNNC.py
@@ -1,69 +1,77 @@
 from string import printable
-from keras.models import  Model,  load_model
-from keras import regularizers
-from keras.layers.core import Dense, Dropout, Lambda
-from keras.layers import Input, ELU,  Embedding, \
-BatchNormalization, Convolution1D,concatenate
-from keras.preprocessing import sequence
-from keras.optimizers import Adam
-from keras import backend as K
-from keras.callbacks import CSVLogger
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import (
+    Dense, Dropout, Lambda, Input, ELU, Embedding,
+    BatchNormalization, Conv1D, concatenate
+)
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import backend as K
+from tensorflow.keras.callbacks import CSVLogger
 from utils import load_model, save_model
-from keras.utils.vis_utils import plot_model
+from tensorflow.keras.utils import plot_model
 
 
 class CNNC:
 
-    def __init__(self, max_len=75, emb_dim=32, max_vocab_len=100, w_reg=regularizers.l2(1e-4)):
+    def __init__(self, max_len=75, emb_dim=32, max_vocab_len=100, w_reg=tf.keras.regularizers.l2(1e-4)):
         self.max_len = max_len
         self.csv_logger = CSVLogger('CNN_log.csv', append=True, separator=';')
+
         main_input = Input(shape=(max_len,), dtype='int32', name='main_input')
+
         # Embedding layer
-        emb = Embedding(input_dim=max_vocab_len, output_dim=emb_dim, input_length=max_len,
-                        W_regularizer=w_reg)(main_input)
+        emb = Embedding(
+            input_dim=max_vocab_len,
+            output_dim=emb_dim,
+            input_length=max_len,
+            embeddings_regularizer=w_reg
+        )(main_input)
         emb = Dropout(0.25)(emb)
 
-        def sum_1d(X):
-            return K.sum(X, axis=1)
+        # Sum function
+        def sum_1d(x):
+            return K.sum(x, axis=1)
 
+        # Convolution function
         def get_conv_layer(emb, kernel_size=5, filters=256):
-            # Conv layer
-            conv = Convolution1D(kernel_size=kernel_size, filters=filters, \
-                                 border_mode='same')(emb)
+            conv = Conv1D(
+                kernel_size=kernel_size,
+                filters=filters,
+                padding='same'
+            )(emb)
             conv = ELU()(conv)
-
-            conv = Lambda(sum_1d, output_shape=(filters,))(conv)
-            # conv = BatchNormalization(mode=0)(conv)
+            conv = Lambda(sum_1d)(conv)
             conv = Dropout(0.5)(conv)
             return conv
 
         # Multiple Conv Layers
+        conv1 = get_conv_layer(emb, kernel_size=2)
+        conv2 = get_conv_layer(emb, kernel_size=3)
+        conv3 = get_conv_layer(emb, kernel_size=4)
+        conv4 = get_conv_layer(emb, kernel_size=5)
 
-        # calling custom conv function from above
-        conv1 = get_conv_layer(emb, kernel_size=2, filters=256)
-        conv2 = get_conv_layer(emb, kernel_size=3, filters=256)
-        conv3 = get_conv_layer(emb, kernel_size=4, filters=256)
-        conv4 = get_conv_layer(emb, kernel_size=5, filters=256)
-
-        # Fully Connected Layers
+        # Merge
         merged = concatenate([conv1, conv2, conv3, conv4], axis=1)
 
+        # Dense layers
         hidden1 = Dense(1024)(merged)
         hidden1 = ELU()(hidden1)
-        hidden1 = BatchNormalization(mode=0)(hidden1)
+        hidden1 = BatchNormalization()(hidden1)
         hidden1 = Dropout(0.5)(hidden1)
 
         hidden2 = Dense(1024)(hidden1)
         hidden2 = ELU()(hidden2)
-        hidden2 = BatchNormalization(mode=0)(hidden2)
+        hidden2 = BatchNormalization()(hidden2)
         hidden2 = Dropout(0.5)(hidden2)
 
-        # Output layer (last fully connected layer)
+        # Output
         output = Dense(1, activation='sigmoid', name='output')(hidden2)
 
-        # Compile model and define optimizer
-        self.model = Model(input=[main_input], output=[output])
-        self.adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+        # Compile model
+        self.model = Model(inputs=[main_input], outputs=[output])
+        self.adam = Adam(learning_rate=1e-4)
         self.model.compile(optimizer=self.adam, loss='binary_crossentropy', metrics=['accuracy'])
 
     def save_model(self, fileModelJSON, fileWeights):
@@ -74,11 +82,17 @@ class CNNC:
         self.model.compile(optimizer=self.adam, loss='binary_crossentropy', metrics=['accuracy'])
 
     def train_model(self, x_train, target_train, epochs=5, batch_size=32):
-        print("Training CNN model with  " + str(epochs) + " epochs and batches of size " + str(batch_size))
-        self.model.fit(x_train, target_train, epochs=epochs, batch_size=batch_size, verbose=1, callbacks=[self.csv_logger])
+        print(f"Training CNN model with {epochs} epochs and batch size {batch_size}")
+        self.model.fit(
+            x_train, target_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            verbose=1,
+            callbacks=[self.csv_logger]
+        )
 
     def test_model(self, x_test, target_test):
-        print("testing CNN model")
+        print("Testing CNN model")
         return self.model.evaluate(x_test, target_test, verbose=1)
 
     def predict(self, x_input):
@@ -88,4 +102,4 @@ class CNNC:
         return "benign" if p < 0.5 else "malicious"
 
     def export_plot(self):
-        plot_model(self.model, to_file='CNN.png')
+        plot_model(self.model, to_file='CNN.png', show_shapes=True)
