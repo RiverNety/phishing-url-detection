4631538323ec4afe3d7e7f59da07a9ab7c2ebd2d
RiverNety <89873015+RiverNety@users.noreply.github.com>
2025-12-22 10:39:16 -0300

    Add train_model to accept validation data in LTSMC
    Updated train_model method to include validation data.

--- LSTMC.py
+++ LSTMC.py
@@ -62,14 +62,15 @@ class LSTMC:
         self.model = load_model(fileModelJSON, fileWeights)
         self.model.compile(optimizer=self.adam, loss='binary_crossentropy', metrics=['accuracy'])
 
-    def train_model(self, x_train, target_train, epochs=5, batch_size=32):
-        print(f"Training LSTM model with {epochs} epochs and batch size {batch_size}")
+    def train_model(self, x_train, target_train, x_val, target_val, epochs=5, batch_size=32):
+        print(f"Training LSTMC model with {epochs} epochs and batch size {batch_size}")
         self.model.fit(
             x_train, target_train,
             epochs=epochs,
             batch_size=batch_size,
             verbose=1,
-            callbacks=[self.csv_logger]
+            callbacks=[self.csv_logger],
+            validation_data=(x_val, target_val)
         )
 
     def test_model(self, x_test, target_test):
