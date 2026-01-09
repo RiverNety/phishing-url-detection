3789bb06ac99918fb97b3059c7c6d6703d861360
RiverNety <89873015+RiverNety@users.noreply.github.com>
2026-01-09 20:50:09 -0300

    Update CNNC.py

--- CNNC.py
+++ CNNC.py
@@ -100,7 +100,12 @@ class CNNC:
         url_int_tokens = [[printable.index(x) + 1 for x in x_input if x in printable]]
         X = sequence.pad_sequences(url_int_tokens, maxlen=self.max_len)
         p = self.model.predict(X, batch_size=1)
-        return "benign",(f"Probabilidade: {p:.4f}") if p < lim else "malicious",(f"Probabilidade: {p:.4f}")
-
+        return "benign" if p < lim else "malicious"
+        
+    def predict_prob(self, x_input, lim=0.5):
+        url_int_tokens = [[printable.index(x) + 1 for x in x_input if x in printable]]
+        X = sequence.pad_sequences(url_int_tokens, maxlen=self.max_len)
+        p = self.model.predict(X, batch_size=1)
+        return p
     def export_plot(self):
         plot_model(self.model, to_file='CNN.png', show_shapes=True)
